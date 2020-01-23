99407b5e11bed05a3768d21abc5f373bffaf7e7b
klguenth <klguenth@gmail.com>
2020-01-22 19:45:33 -0600

    fixed migration error

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,5 +4,5 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DB_URL,
-  "validateChecksums": true, // Set to false to skip validation
+  "validateChecksums": false, // Set to false to skip validation
 }
