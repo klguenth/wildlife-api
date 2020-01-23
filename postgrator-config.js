0d897815e5730b1ddb478dede1cd900b93a450c7
klguenth <klguenth@gmail.com>
2020-01-22 19:23:21 -0600

    postgrator fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,4 +4,5 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DB_URL,
+  "validateChecksums": true, // Set to false to skip validation
 }
