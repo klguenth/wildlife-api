d77e77200a6b0f9b2614dda1e71dc638265a583f
klguenth <klguenth@gmail.com>
2020-02-23 10:41:52 -0600

    update password auth

--- src/users-service.js
+++ src/users-service.js
@@ -23,7 +23,7 @@ const UsersService = {
     if (password.length < 3) {
       return 'Password must be longer than 3 characters'
     }
-    if (password.length > 72) {
+    if (password.length < 72) {
       return 'Password must be less than 72 characters'
     }
     if (password.startsWith(' ') || password.endsWith(' ')) {
