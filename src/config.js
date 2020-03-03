8eabac804e50d6bb498efa3c84cea57307d01935
klguenth <klguenth@gmail.com>
2020-03-02 21:52:22 -0600

    update

--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://wildlife@localhost/wildlife',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://wildlife@localhost/wildlife-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT
 };
